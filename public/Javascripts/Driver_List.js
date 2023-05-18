4e6d881fa802f6a8b17cfc7a39fb92c043e327bd
Shivang-Dhimmar <shivang3921dhimmar@gmail.com>
2023-05-19 00:42:58 +0530

    for Submit

--- public/Javascripts/Driver_List.js
+++ public/Javascripts/Driver_List.js
@@ -21,15 +21,11 @@ window.onload = async () => {
     var row = table.insertRow();
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
     // const data = `<tr><td>${entry.name}</td><td>${entry.phoneNo}</td>
     // <td>${entry.name}</td><td>${entry.name}</td></tr>`;
     // table.innerHTML += data;
     cell1.innerHTML = entry.name;
     cell2.innerHTML = entry.phoneNo;
-    cell3.innerHTML = "3";
-    cell4.innerHTML = "4";
   }
   console.log(passengers);
 };

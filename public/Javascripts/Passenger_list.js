ac83d201f71dcab0201dddef439caa7213d00d5a
Decostar123 <decostarsharma113@gmail.com>
2023-05-19 21:12:03 +0530

    final commit

--- public/Javascripts/Passenger_list.js
+++ public/Javascripts/Passenger_list.js
@@ -24,6 +24,49 @@ logOut.addEventListener('click' , async () =>{
   }
     
 })
+
+function message(){
+  let arr = [] ; 
+ alert(" message shoeinf ") ; 
+ 
+ fetch(DRIVER_LIST).then( res => res.json())
+ .then( data => { 
+   arr = data.result ; 
+   console.log( data.result)
+   alert(arr) ; 
+ const table = document.querySelector("#tables").getElementsByTagName('tbody')[0];
+  for (entry of arr ) {
+         var row = table.insertRow();
+         var cell1 = row.insertCell(0);
+         var cell2 = row.insertCell(1);
+         // const data = `<tr><td>${entry.name}</td><td>${entry.phoneNo}</td>
+         // <td>${entry.name}</td><td>${entry.name}</td></tr>`;
+         // table.innerHTML += data;
+         cell1.innerHTML = entry.name;
+         cell2.innerHTML = entry.phoneNo;
+       } 
+
+ 
+ 
+ 
+ 
+ } 
+    ) ; 
+   // const data = await resp.json();
+   // const passengers = data.result;
+   // for (entry of passengers) {
+   //       var row = table.insertRow();
+   //       var cell1 = row.insertCell(0);
+   //       var cell2 = row.insertCell(1);
+   //       // const data = `<tr><td>${entry.name}</td><td>${entry.phoneNo}</td>
+   //       // <td>${entry.name}</td><td>${entry.name}</td></tr>`;
+   //       // table.innerHTML += data;
+   //       cell1.innerHTML = entry.name;
+   //       cell2.innerHTML = entry.phoneNo;
+   //     } 
+   // console.log( passengers ) ; 
+
+}
 // window.addEventListener( "load" ,  async (event ) => {
 //   const table = document.querySelector("#table");
 //   // console.log(table.innerHTML, "44444");

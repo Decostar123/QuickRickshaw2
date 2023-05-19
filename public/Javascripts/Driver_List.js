617cd84f7d166fc46371365c4841186e15377051
Decostar123 <decostarsharma113@gmail.com>
2023-05-19 19:55:37 +0530

    final commit

--- public/Javascripts/Driver_List.js
+++ public/Javascripts/Driver_List.js
@@ -28,27 +28,29 @@ const PASSENGER_LIST = "https://quickrickshaws.onrender.com/driver/passengers";
 const body = document.getElementsByTagName("body")[0];
 body.addEventListener("load", async () => {
   // console.log("Hello World!");
-  const table = document.querySelector("#tables").getElementsByTagName('tbody')[0];
+  // const table = document.querySelector("#tables").getElementsByTagName('tbody')[0];
 
-    // const table = document.querySelector("#table");
-  // console.log(table.innerHTML, "44444");
-  // console.log(table);
-  const resp = await fetch(PASSENGER_LIST);
-  const data = await resp.json();
-  const passengers = data.result;
-  console.log( "the list of passengers " , passengers ) ; 
+  //   // const table = document.querySelector("#table");
+  // // console.log(table.innerHTML, "44444");
+  // // console.log(table);
+  // const resp = await fetch(PASSENGER_LIST);
+  // const data = await resp.json();
+  // const passengers = data.result;
+  // console.log( "the list of passengers " , passengers ) ; 
 
-  for (entry of passengers) {
-    var row = table.insertRow();
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    // const data = `<tr><td>${entry.name}</td><td>${entry.phoneNo}</td>
-    // <td>${entry.name}</td><td>${entry.name}</td></tr>`;
-    // table.innerHTML += data;
-    cell1.innerHTML = entry.name;
-    cell2.innerHTML = entry.phoneNo;
-  }
-  console.log(passengers);
+  // for (entry of passengers) {
+  //   var row = table.insertRow();
+  //   var cell1 = row.insertCell(0);
+  //   var cell2 = row.insertCell(1);
+  //   // const data = `<tr><td>${entry.name}</td><td>${entry.phoneNo}</td>
+  //   // <td>${entry.name}</td><td>${entry.name}</td></tr>`;
+  //   // table.innerHTML += data;
+  //   cell1.innerHTML = entry.name;
+  //   cell2.innerHTML = entry.phoneNo;
+  // }
+  // console.log(passengers);
+
+  console.log(  " the body has laoded ") ; 
 
 
   // alert("will start fetching ") ;
